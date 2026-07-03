a0ddd2879a9f2c6f5b71544be3d9d3049fcbe00f
Duyen1004 <duyenhthe181489@fpt.edu.vn>
2026-07-03 16:28:05 +0700

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/MLN131-SPST/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
